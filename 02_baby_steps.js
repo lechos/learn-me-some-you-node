9f882ca0feac5d28a024fbd3c455b52bf50436e6
lechos <linneasaltel@gmail.com>
2016-03-15 04:26:30 +0000

    first commit

---
+++ 02_baby_steps.js
@@ -0,0 +1,11 @@
+// (function (){
+  var nums = process.argv.slice(2);
+  
+  var total = nums.reduce(function(total, i) {
+    total += parseInt(i);
+    return total;
+  }, 0);
+
+console.log(total);
+// });
+//console.log(process.argv)
